c6d65e0ca385f53d43960ecc1671d95cfab92aae
Srujan Deshpande <srujan.deshpande@gmail.com>
2020-05-09 00:13:15 +0530

    New Record function

--- contracts/Healthnet.sol
+++ contracts/Healthnet.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.5.0;
 
 contract Healthnet {
+
   uint public patientCount = 0;
   uint public hospitalCount = 0;
   uint public insurerCount = 0;
@@ -15,20 +16,22 @@ contract Healthnet {
   }
 
   struct HospitalData {
+    address hospitalAddress;
     string name;
     string code;
   }
 
   struct InsurerData {
+    address insurerAddress;
     string name;
     string code;
   }
 
   struct RecordData {
     uint patientNumber;
+    address hospitalAddress;
     string recordText;
     string dateTime;
-    uint hospitalNumber;
     uint next;
     uint approved;
   }
@@ -36,15 +39,25 @@ contract Healthnet {
   mapping(uint => PatientData) public patientList;
   mapping(uint => HospitalData) public hospitalList;
   mapping(uint => InsurerData) public insurerList;
+  mapping(uint => RecordData) public recordList;
 
   event NewRecord(
+    uint recordNumber,
     uint patientNumber,
+    address hospitalAddress,
     string recordText,
     string dateTime
   );
 
   constructor() public {
     // Can make the constructor do something here.
+  }
+
+  function addRecord(uint _patient_number, string memory _record_text, string memory _date_time, uint _hospitalnumber) public {
+    address _hospital_address = msg.sender;
+    recordCount ++;
+    record = RecordData(_patient_number, _hospital_Address, _record_text, _date_time);
+    emit NewRecord(recordCount, _patient_number, _hospital_address, _record_text, _date_time);
   }
 
   function markPresent(string memory _name, string memory _date) public {
